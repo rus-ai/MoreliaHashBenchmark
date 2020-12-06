640717d964d83962cf1522d9ead6f4f6a04f83db
rus-ai <russian-ai@yandex.ru>
2020-12-06 22:10:55 +0300

    Prepare for release

--- app/src/main/java/ru/wtw/moreliahashbenchmark/MainActivity.java
+++ app/src/main/java/ru/wtw/moreliahashbenchmark/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +19,6 @@ import java.nio.charset.Charset;
 
 import static com.theromus.sha.Parameters.KECCAK_256;
 import static com.theromus.sha.Parameters.SHA3_256;
-
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -49,6 +49,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         TextView editIterCount = findViewById(R.id.editTextIterCount);
         EditText editResult = findViewById(R.id.editResult);
+        Spinner algoSelect = findViewById(R.id.spinnerAlgo);
+        String selected_val=algoSelect.getSelectedItem().toString();
         maxDegreeCount = Integer.parseInt(editIterCount.getText().toString());
 
         for (degreeCounter = 0; degreeCounter < maxDegreeCount; degreeCounter++ ) {
@@ -56,44 +58,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             for (int i = 1; i <= degreeCounter; i++) {
                 maxCounter *= 10;
             }
-            editResult.setText(editResult.getText()+"\n"+"Number of iteration: "+String.valueOf(maxCounter));
+            editResult.setText(editResult.getText() + "\n" + "Number of iteration: " + String.valueOf(maxCounter));
             algo = "KECCAK_256";
             data = "The quick brown fox jumps over the lazy dog".getBytes(Charset.forName("UTF-8"));
-            startTime = System.currentTimeMillis();
-            for (counter = 0; counter < maxCounter; counter++) {
-                data = keccak.getHash(data, KECCAK_256);
+            if (selected_val.equals("ALL") || selected_val.equals(algo)) {
+                startTime = System.currentTimeMillis();
+                for (counter = 0; counter < maxCounter; counter++) {
+                    data = keccak.getHash(data, KECCAK_256);
+                }
+                stopTime = System.currentTimeMillis();
+                editResult.setText(editResult.getText() + "\n" + algo + " time " + String.valueOf(stopTime - startTime) + " ms");
             }
-            stopTime = System.currentTimeMillis();
-            editResult.setText(editResult.getText()+"\n"+algo+" time "+String.valueOf(stopTime-startTime)+" ms");
             algo = "SHA3_256";
             data = "The quick brown fox jumps over the lazy dog".getBytes(Charset.forName("UTF-8"));
-            startTime = System.currentTimeMillis();
-            for (counter = 0; counter < maxCounter; counter++) {
-                data = keccak.getHash(data, SHA3_256);
+            if (selected_val.equals("ALL") || selected_val.equals(algo)) {
+                startTime = System.currentTimeMillis();
+                for (counter = 0; counter < maxCounter; counter++) {
+                    data = keccak.getHash(data, SHA3_256);
+                }
+                stopTime = System.currentTimeMillis();
+                editResult.setText(editResult.getText() + "\n" + algo + " time " + String.valueOf(stopTime - startTime) + " ms");
             }
-            stopTime = System.currentTimeMillis();
-            editResult.setText(editResult.getText()+"\n"+algo+" time "+String.valueOf(stopTime-startTime)+" ms");
             algo = "blake2b";
             data = "The quick brown fox jumps over the lazy dog".getBytes(Charset.forName("UTF-8"));
-            startTime = System.currentTimeMillis();
-            for (counter = 0; counter < maxCounter; counter++) {
-                blake2b.update(data);
-                data=blake2b.digest();
+            if (selected_val.equals("ALL") || selected_val.equals(algo)) {
+                startTime = System.currentTimeMillis();
+                for (counter = 0; counter < maxCounter; counter++) {
+                    blake2b.update(data);
+                    data = blake2b.digest();
+                }
+                stopTime = System.currentTimeMillis();
+                editResult.setText(editResult.getText() + "\n" + algo + " time " + String.valueOf(stopTime - startTime) + " ms");
             }
-            stopTime = System.currentTimeMillis();
-            editResult.setText(editResult.getText()+"\n"+algo+" time "+String.valueOf(stopTime-startTime)+" ms");
-
             algo = "blake2s";
             data = "The quick brown fox jumps over the lazy dog".getBytes(Charset.forName("UTF-8"));
-            startTime = System.currentTimeMillis();
-            for (counter = 0; counter < maxCounter; counter++) {
-                blake2s.update(data);
-                data=blake2s.digest();
+            if (selected_val.equals("ALL") || selected_val.equals(algo)) {
+                startTime = System.currentTimeMillis();
+                for (counter = 0; counter < maxCounter; counter++) {
+                    blake2s.update(data);
+                    data = blake2s.digest();
+                }
+                stopTime = System.currentTimeMillis();
+                editResult.setText(editResult.getText() + "\n" + algo + " time " + String.valueOf(stopTime - startTime) + " ms");
             }
-            stopTime = System.currentTimeMillis();
-            editResult.setText(editResult.getText()+"\n"+algo+" time "+String.valueOf(stopTime-startTime)+" ms");
-
         }
-
     }
 }
